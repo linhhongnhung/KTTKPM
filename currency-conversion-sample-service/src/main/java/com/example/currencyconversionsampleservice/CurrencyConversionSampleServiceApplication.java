2f0be0697c6064ac9b6bf796c432325fe9b6f41c
Linh Hong Nhung <linhhongnhung11@gmail.com>
2023-06-07 20:06:27 +0700

    hihi

---
+++ currency-conversion-sample-service/src/main/java/com/example/currencyconversionsampleservice/CurrencyConversionSampleServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.currencyconversionsampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionSampleServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConversionSampleServiceApplication.class, args);
+    }
+}
---
+++ currency-exchange-sample-service/src/test/java/com/example/currencyexchangesampleservice/CurrencyExchangeSampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyexchangesampleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeSampleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
